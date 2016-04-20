83f8663290553c9fa2f045c36c78e46fbbb4617b
Igor Ivaniuk <i.ivanyuk@gmail.com>
2016-04-20 23:54:21 +0200

    Scala slick codegen

---
+++ simple/src/main/scala/com/zpl/model/CodeGenerator.scala
@@ -0,0 +1,20 @@
+package com.zpl.model
+
+import slick.codegen.SourceCodeGenerator
+
+/**
+  * Created by IgorIvaniuk on 20.04.2016.
+  */
+object CodeGenerator extends App {
+  val outputDir = ".//src/main/scala";
+  val username = "user";
+  val password = "user";
+  val url = "jdbc:mysql://localhost/zpl";
+  val jdbcDriver = "com.mysql.jdbc.Driver";
+  val slickDriver = "slick.driver.MySQLDriver";
+  val pkg = "com.zpl.model";
+
+  SourceCodeGenerator.main(
+    Array(slickDriver, jdbcDriver, url, outputDir, pkg, username, password)
+  )
+}
