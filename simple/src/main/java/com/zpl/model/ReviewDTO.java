562482ae4e5e3dab5658ca1b0ddbe2d709126c4b
Igor Ivaniuk <i.ivanyuk@gmail.com>
2016-07-23 19:49:22 +0200

    Removed lombok builder - causing problems during compile

--- simple/src/main/java/com/zpl/model/ReviewDTO.java
+++ simple/src/main/java/com/zpl/model/ReviewDTO.java
@@ -1,7 +1,6 @@
 package com.zpl.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -9,7 +8,6 @@ import lombok.Data;
  */
 @Data
 @AllArgsConstructor
-@Builder
 public class ReviewDTO {
 
     private Integer id;
@@ -17,4 +15,45 @@ public class ReviewDTO {
     private Integer rating;
     private String reviewText;
 
+    public static ReviewDTOBuilder builder() {
+        return new ReviewDTOBuilder();
+    }
+
+    public static class ReviewDTOBuilder {
+        private Integer id;
+        private Integer customerId;
+        private Integer rating;
+        private String reviewText;
+
+        ReviewDTOBuilder() {
+        }
+
+        public ReviewDTO.ReviewDTOBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public ReviewDTO.ReviewDTOBuilder customerId(Integer customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public ReviewDTO.ReviewDTOBuilder rating(Integer rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public ReviewDTO.ReviewDTOBuilder reviewText(String reviewText) {
+            this.reviewText = reviewText;
+            return this;
+        }
+
+        public ReviewDTO build() {
+            return new ReviewDTO(id, customerId, rating, reviewText);
+        }
+
+        public String toString() {
+            return "com.zpl.model.ReviewDTO.ReviewDTOBuilder(id=" + this.id + ", customerId=" + this.customerId + ", rating=" + this.rating + ", reviewText=" + this.reviewText + ")";
+        }
+    }
 }
